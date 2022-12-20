89834fe5e49aea5b246ac95c781a5eb8b7379a50
Shah-Maqsood <koelnbonn99@gmail.com>
2022-12-20 03:14:11 +0100

    Features has been updated into the feature class 02

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+
+        Features features=new Features();
+        features.color="black";
+        features.year=1999;
+        features.price=200000;
+
         System.out.println("Hello world!");
     }
 }
